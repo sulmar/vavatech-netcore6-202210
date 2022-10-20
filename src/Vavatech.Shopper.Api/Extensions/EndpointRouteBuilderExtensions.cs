d27f94f8aaddc8a6f4e9ad5be364062b8157b5a2
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-10-20 10:38:15 +0200

    Dodanie MapHead i MapPatch

---
+++ src/Vavatech.Shopper.Api/Extensions/EndpointRouteBuilderExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Vavatech.Shopper.Api.Extensions
+{
+    public static class EndpointRouteBuilderExtensions
+    {
+        private static readonly string[] HeadVerb = new[] { "HEAD" };
+        private static readonly string[] PatchVerb = new[] { "PATCH" };
+
+        public static IEndpointConventionBuilder MapHead(this IEndpointRouteBuilder endpoints, 
+            string pattern, Delegate requestDelegate)
+        {
+            return endpoints.MapMethods(pattern, HeadVerb, requestDelegate);
+        }
+
+        public static IEndpointConventionBuilder MapPatch(this IEndpointRouteBuilder endpoints,
+            string pattern, Delegate requestDelegate)
+        {
+            return endpoints.MapMethods(pattern, PatchVerb, requestDelegate);
+        }
+    }
+}
