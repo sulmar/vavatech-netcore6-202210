b3630a78846d3a2663de6321fba9ea17bd62be4f
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-10-20 16:25:17 +0200

    Dodanie konfiguracji

--- src/Vavatech.Shopper.Api/Program.cs
+++ src/Vavatech.Shopper.Api/Program.cs
@@ -6,7 +6,38 @@ using Vavatech.Shopper.Domain.Validators;
 
 // var app = WebApplication.Create();
 
-var builder = WebApplication.CreateBuilder();
+var builder = WebApplication.CreateBuilder(args);
+
+// SETX ASPNETCORE_ENVIRONMENT="Testing"
+string environmentName = builder.Environment.EnvironmentName;
+
+if (builder.Environment.IsEnvironment("Testing"))
+{
+
+}
+
+// Domyœlni providerzy
+builder.Configuration.AddJsonFile("appsettings.json", optional: false);
+builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true);
+builder.Configuration.AddEnvironmentVariables(); // --ASPNETCORE_NbpApi=USD 
+builder.Configuration.AddCommandLine(args); // --NbpApi__Code=USD
+
+// Dodatkowe
+builder.Configuration.AddXmlFile("appsettings.xml", optional: true);
+builder.Configuration.AddIniFile("appsettings.ini", optional: true);
+
+builder.Configuration.AddInMemoryCollection(new Dictionary<string, string>
+    {
+        { "NbpApi:Code", "CHF" },
+        { "NbpApi:Table", "A" },
+    }
+);
+
+builder.Configuration.AddEnvironmentVariables("API"); // --API_NbpApi = USD
+
+// Hosting
+// https://weblog.west-wind.com/posts/2016/Jun/06/Publishing-and-Running-ASPNET-Core-Applications-with-IIS#running-iis-as-a-development-server-no
+
 builder.Services.AddSingleton<ICustomerRepository, InMemoryCustomerRepository>(); // rejestracja us³ugi w kontenerze wstrzykiwania zale¿noœci (Dependency Injection)
 
 builder.Services.AddTransient<IValidator<Customer>, CustomerValidator>();
