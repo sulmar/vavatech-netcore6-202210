370351f3fd7145dd1b4694b0c272ee6c48f9c15b
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-10-21 11:17:42 +0200

    Dodanie walidatorów

---
+++ src/Vavatech.Shopper.Domain/Validators/PaymentValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Vavatech.Shopper.Domain.Validators
+{
+    public class PaymentValidator : AbstractValidator<Payment>
+    {
+        public PaymentValidator()
+        {
+            RuleFor(p => p.Amount).NotEmpty().InclusiveBetween(1, 10000);
+        }
+    }
+}
---
+++ src/Vavatech.Shopper.Domain/Validators/ProductValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Vavatech.Shopper.Domain.Validators
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty();
+            RuleFor(p => p.Color).NotEmpty();
+            RuleFor(p => p.Price).InclusiveBetween(1, 1000);
+        }
+    }
+}
