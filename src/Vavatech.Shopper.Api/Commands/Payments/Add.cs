b0c45e344103443b0d9753e410e5f6ae84f8c7c0
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-10-21 11:08:07 +0200

    Dodanie AddPaymentCommand

---
+++ src/Vavatech.Shopper.Api/Commands/Payments/Add.cs
@@ -0,0 +1,24 @@
+﻿using Ardalis.ApiEndpoints;
+using Microsoft.AspNetCore.Mvc;
+using Vavatech.Shopper.Api.Queries.Payments;
+
+namespace Vavatech.Shopper.Api.Commands.Payments
+{
+    public class AddPaymentCommand : EndpointBaseAsync.WithRequest<Payment>.WithActionResult<Payment>
+    {
+        private readonly IPaymentRepository paymentRepository;
+
+        public AddPaymentCommand(IPaymentRepository paymentRepository)
+        {
+            this.paymentRepository = paymentRepository;
+        }
+
+        [HttpPost("api/payments")]
+        public async override Task<ActionResult<Payment>> HandleAsync(Payment payment, CancellationToken cancellationToken = default)
+        {
+            paymentRepository.Add(payment);
+
+            return CreatedAtRoute(nameof(GetPaymentByIdQuery), new { Id = payment.Id }, payment);
+        }
+    }
+}
--- src/Vavatech.Shopper.Api/Queries/Payments/GetById.cs
+++ src/Vavatech.Shopper.Api/Queries/Payments/GetById.cs
@@ -3,16 +3,16 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Vavatech.Shopper.Api.Queries.Payments
 {
-    public class GetById : EndpointBaseAsync.WithRequest<int>.WithResult<Payment>
+    public class GetPaymentByIdQuery : EndpointBaseAsync.WithRequest<int>.WithResult<Payment>
     {
         private readonly IPaymentRepository paymentRepository;
 
-        public GetById(IPaymentRepository paymentRepository)
+        public GetPaymentByIdQuery(IPaymentRepository paymentRepository)
         {
             this.paymentRepository = paymentRepository;
         }
 
-        [HttpGet("/api/payments/{id}")]
+        [HttpGet("/api/payments/{id}", Name = nameof(GetPaymentByIdQuery))]
         public override Task<Payment> HandleAsync(int id, CancellationToken cancellationToken = default)
         {
             var payment = paymentRepository.Get(id);
